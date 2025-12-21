3dd132a9956ee18262221e157305ad9ad6116ddd
mohit <mohit44790@gmail.com>
2025-12-21 14:48:24 +0530

    StudentProfile

--- admission/src/main/java/com/mohit44790/admission/entity/StudentProfile.java
+++ admission/src/main/java/com/mohit44790/admission/entity/StudentProfile.java
@@ -1,4 +1,23 @@
 package com.mohit44790.admission.entity;
 
+
+import jakarta.persistence.*;
+
+@Entity
 public class StudentProfile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private User user;
+
+    private String name;
+    private String alternateMobile;
+    private String address;
+    private boolean profileCompleted;
+
+    // getters & setters
 }
+
