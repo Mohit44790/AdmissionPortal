b332f4cf5291b74cd79afa0b17714e9387f65054
mohit <mohit44790@gmail.com>
2025-12-21 14:49:20 +0530

    StudentDocument

--- admission/src/main/java/com/mohit44790/admission/entity/StudentDocument.java
+++ admission/src/main/java/com/mohit44790/admission/entity/StudentDocument.java
@@ -1,4 +1,19 @@
 package com.mohit44790.admission.entity;
 
+import jakarta.persistence.*;
+
+
+@Entity
 public class StudentDocument {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String documentType;
+    private String filePath;
+
+    @ManyToOne
+    private StudentProfile studentProfile;
 }
+
