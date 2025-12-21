7db9df6339fee65a6d3c7b4e66abedc37f923147
mohit <mohit44790@gmail.com>
2025-12-21 14:45:17 +0530

    user

--- admission/src/main/java/com/mohit44790/admission/entity/User.java
+++ admission/src/main/java/com/mohit44790/admission/entity/User.java
@@ -1,4 +1,27 @@
 package com.mohit44790.admission.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String mobile;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private boolean verified = true;
+
+    // getters & setters
 }
