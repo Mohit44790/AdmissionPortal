4e8e5ec15df3af4c95933986b6a5b3d11ac1ef15
mohit <mohit44790@gmail.com>
2025-12-22 22:47:52 +0530

    course repo

--- admission/src/main/java/com/mohit44790/admission/repository/ProgramRepository.java
+++ admission/src/main/java/com/mohit44790/admission/repository/ProgramRepository.java
@@ -3,5 +3,9 @@ package com.mohit44790.admission.repository;
 import com.mohit44790.admission.entity.Program;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProgramRepository extends JpaRepository<Program,Long> {}
+import java.util.Optional;
 
+public interface ProgramRepository extends JpaRepository<Program, Long> {
+
+    Optional<Program> findByLevel(String level);
+}
