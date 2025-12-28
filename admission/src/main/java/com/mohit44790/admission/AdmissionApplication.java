5f43e14f855d95d615e6d84f80893f15729b81c0
mohit <mohit44790@gmail.com>
2025-12-28 20:33:36 +0530

    doc

--- admission/src/main/java/com/mohit44790/admission/AdmissionApplication.java
+++ admission/src/main/java/com/mohit44790/admission/AdmissionApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdmissionApplication {
+public class 	AdmissionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdmissionApplication.class, args);
