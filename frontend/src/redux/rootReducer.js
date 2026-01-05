83b26b1a496ec0e3576a49779ebcc7108428dc23
mohit <mohit44790@gmail.com>
2026-01-05 10:06:50 +0530

    upload document view

--- frontend/src/redux/rootReducer.js
+++ frontend/src/redux/rootReducer.js
@@ -2,12 +2,13 @@ import { combineReducers } from "redux";
 import authReducer from "./slices/authSlice";
 import studentProfileReducer from "./slices/studentSlice";
 import admissionReducer from "./slices/admissionSlice";
-
+import admissionDocumentReducer from "./slices/admissionDocumentSlice";
 // Combine reducers
 const rootReducer = combineReducers({
   auth: authReducer,
   studentProfile: studentProfileReducer,
   admission: admissionReducer,
+  admissionDocument: admissionDocumentReducer,
  
  
 });
