ff89314ac5c59f9d2c53130ef1372cd5298133a7
mohit <mohit44790@gmail.com>
2026-01-05 20:34:18 +0530

    admisssiion

--- frontend/src/redux/slices/admissionDocumentSlice.js
+++ frontend/src/redux/slices/admissionDocumentSlice.js
@@ -21,4 +21,54 @@ export const uploadDocument = createAsyncThunk(
             
         }
     }
-)
+);
+ 
+// get document
+
+export const fetchDocuments = createAsyncThunk(
+    "admissionDocument/fetchAll",
+    async (__, {rejectWithValue})=>{
+        try {
+            const res = await api.get("student/admission/documents");
+            return res.data;
+        } catch (error) {
+            return rejectWithValue(error.response?.data?.message || "Failed to fetch documents")
+            
+        }
+    }
+);
+
+// view document 
+export const viewDocument = createAsyncThunk(
+  "admissionDocument/view",
+  async (id, { rejectWithValue }) => {
+    try {
+      const res = await api.get(
+        `/student/admission/view-document/${id}`,
+        { responseType: "blob" }
+      );
+      return { blob: res.data, id };
+    } catch (err) {
+      return rejectWithValue(
+        err.response?.data?.message || "Failed to view document"
+      );
+    }
+  }
+);
+
+
+const admissionDocumentSlice = createSlice({
+    name:"admissionDocument",
+    initialState:{
+        document:[],
+        loading:false,
+        error:null,
+        viewing:null,
+    },
+    reducers:{
+        clearView:(state)=>{
+            state.viewing = null;
+        }
+    }
+})
+
