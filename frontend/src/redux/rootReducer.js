0c8fbc84e7fa94fa52acb4aaf9429c55331eca8e
mohit <mohit44790@gmail.com>
2026-01-06 09:30:11 +0530

    master

--- frontend/src/redux/rootReducer.js
+++ frontend/src/redux/rootReducer.js
@@ -3,12 +3,15 @@ import authReducer from "./slices/authSlice";
 import studentProfileReducer from "./slices/studentSlice";
 import admissionReducer from "./slices/admissionSlice";
 import admissionDocumentReducer from "./slices/admissionDocumentSlice";
+import masterReducer from "./slices/masterSlice";
+
 // Combine reducers
 const rootReducer = combineReducers({
   auth: authReducer,
   studentProfile: studentProfileReducer,
   admission: admissionReducer,
   admissionDocument: admissionDocumentReducer,
+   master: masterReducer,
  
  
 });
