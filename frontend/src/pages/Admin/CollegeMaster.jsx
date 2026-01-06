c66d3890ec665b0930f690a365500a38e070c42a
mohit <mohit44790@gmail.com>
2026-01-06 22:41:05 +0530

    Admin page

---
+++ frontend/src/pages/Admin/CollegeMaster.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CollegeMaster = () => {
+  return (
+    <div>CollegeMaster</div>
+  )
+}
+
+export default CollegeMaster
---
+++ frontend/src/pages/Admin/ProgramMaster.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProgramMaster = () => {
+  return (
+    <div>ProgramMaster</div>
+  )
+}
+
+export default ProgramMaster
