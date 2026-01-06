9dea2cc2acae8196dc33c86b5baffd721717de24
mohit <mohit44790@gmail.com>
2026-01-06 22:20:42 +0530

    course

--- frontend/src/redux/slices/masterSlice.js
+++ frontend/src/redux/slices/masterSlice.js
@@ -1,31 +1,43 @@
-import { createSlice ,createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import api from "../services/api";
 
-//create program 
-
+/* ================= PROGRAM ================= */
 export const createProgram = createAsyncThunk(
-    "master/createProgram",
-    async(data,{rejectWithValue}) =>{
-        try {
-            const res = await api.post("/api/master/admin/program",data);
-            return res.data;
-        } catch (error) {
-            return rejectWithValue(error.response?.data?.message || "Program create failed")
-        }
+  "master/createProgram",
+  async (data, { rejectWithValue }) => {
+    try {
+      const res = await api.post("/api/master/admin/program", data);
+      return res.data;
+    } catch (e) {
+      return rejectWithValue(e.response?.data?.message || "Program create failed");
     }
+  }
 );
 
 export const deleteProgram = createAsyncThunk(
-    "master/deleteProgram",
-    async(id,{rejectWithValue})=>{
-        try {
-            await api.delete(`api/master/admin/program/${id}`)
-            return id;
-        } catch (error) {
-             return rejectWithValue("Delete failed");
-        }
+  "master/deleteProgram",
+  async (id, { rejectWithValue }) => {
+    try {
+      await api.delete(`/api/master/admin/program/${id}`);
+      return id;
+    } catch (e) {
+      return rejectWithValue("Delete failed");
     }
-)
+  }
+);
+
+/* ================= COLLEGE ================= */
+export const createCollege = createAsyncThunk(
+  "master/createCollege",
+  async (data, { rejectWithValue }) => {
+    try {
+      const res = await api.post("/api/master/admin/college", data);
+      return res.data;
+    } catch (e) {
+      return rejectWithValue("College create failed");
+    }
+  }
+);
 
 /* ================= COURSE ================= */
 export const createCourse = createAsyncThunk(
@@ -39,3 +51,4 @@ export const createCourse = createAsyncThunk(
     }
   }
 );
+
