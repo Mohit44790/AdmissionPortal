f7d47cb3bb04f897e1c1a4da2e06b6a0c2a293b1
mohit <mohit44790@gmail.com>
2026-01-07 10:28:33 +0530

    college page

--- frontend/src/pages/Admin/CollegeMaster.jsx
+++ frontend/src/pages/Admin/CollegeMaster.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { createCollege } from '../../redux/slices/masterSlice';
 
 const CollegeMaster = () => {
+    const [collegeName ,setCollegeName] = useState("");
+    const dispatch = useDispatch();
+
   return (
-    <div>CollegeMaster</div>
+    <div className='bg-white p-6 rounded-xl shadow'>
+        <h2 className='text-xl font-bold'>Create College</h2>
+         <input  value={collegeName}
+         onChange={(e) => setCollegeName(e.target.value)} placeholder='CollegeName' className="border px-3 py-2 rounded w-full mb-3"/>
+         <button onClick={()=>dispatch(createCollege({collegeName}))} className='bg-green-400 text-white px-6 py-2 rounded'>Save College</button>
+    </div>
   )
 }
 
